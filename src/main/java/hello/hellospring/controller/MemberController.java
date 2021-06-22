7eccaaf43d463cd134795fcb5b1d45cf07331945
guhan <guhan589@naver.com>
2021-06-23 00:31:56 +0900

    DI 의존성 주입 교육

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) { //dependency injection 의존성 주입
+        this.memberService = memberService;
+    }
+}
